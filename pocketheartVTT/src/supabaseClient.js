9a0da691e7e9e24dddf7e4024d839d07d82009d3
Victoevel <victoeyou@gmail.com>
2026-04-09 13:42:18 -0600

    Delete /supabaseClient.js

--- pocketheartVTT/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://bkdtayppxvzzjlwsswbo.supabase.co'
-const supabaseKey = 'sb_publishable__81mrTjebmXQaDI4uD-bPQ_Zp0_DSJ6'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
